9f2402168eded8e384ce6d2ac919de77e21c7ac4
Jean Laneurit <jean.laneurit@inrae.fr>
2023-10-30 16:28:51 +0100

    Initial commit

---
+++ romea_camera_bringup/python/romea_camera_bringup.py
@@ -0,0 +1,122 @@
+# Copyright 2022 INRAE, French National Research Institute for Agriculture, Food and Environment
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from romea_common_bringup import MetaDescription, robot_urdf_prefix, device_namespace
+import romea_camera_description
+from numpy import radians, deg2rad
+
+
+class CameraMetaDescription:
+    def __init__(self, meta_description_file_path):
+        self.meta_description = MetaDescription(
+            "camera", meta_description_file_path)
+
+    def get_name(self):
+        return self.meta_description.get("name")
+
+    def get_namespace(self):
+        return self.meta_description.get_or("namespace", None)
+
+    def has_driver_configuration(self):
+        return self.meta_description.exists("driver")
+
+    def get_driver_pkg(self):
+        return self.meta_description.get("pkg", "driver")
+
+    def get_type(self):
+        return self.meta_description.get("type", "configuration")
+
+    def get_model(self):
+        return self.meta_description.get("model", "configuration")
+
+    def get_frame_rate(self):
+        return self.meta_description.get_or("frame_rate", "configuration", None)
+
+    def get_resolution(self):
+        return self.meta_description.get_or("resolution", "configuration", None)
+
+    def get_horizontal_fov(self):
+        return self.meta_description.get_or("horizontal_fov", "configuration", None)
+
+    def get_video_format(self):
+        return self.meta_description.get_or("video_format", "configuration", None)
+
+    def get_parent_link(self):
+        return self.meta_description.get("parent_link", "geometry")
+
+    def get_xyz(self):
+        return self.meta_description.get("xyz", "geometry")
+
+    def get_rpy_deg(self):
+        return self.meta_description.get("rpy", "geometry")
+
+    def get_rpy_rad(self):
+        return radians(self.get_rpy_deg()).tolist()
+
+    def get_records(self):
+        return self.meta_description.get_or("records", None,  {})
+
+    def get_bridge(self):
+        return self.meta_description.get_or("bridge", None,  {})
+
+
+def load_meta_description(meta_description_file_path):
+    return CameraMetaDescription(meta_description_file_path)
+
+
+def get_sensor_specifications(meta_description):
+    return romea_camera_description.get_camera_specifications(
+        meta_description.get_type(), meta_description.get_model()
+    )
+
+
+def get_sensor_geometry(meta_description):
+    return romea_camera_description.get_camera_geometry(
+        meta_description.get_type(), meta_description.get_model()
+    )
+
+
+def urdf_description(robot_namespace, mode, meta_description_file_path):
+
+    meta_description = CameraMetaDescription(meta_description_file_path)
+
+    ros_namespace = device_namespace(
+        robot_namespace,
+        meta_description.get_namespace(),
+        meta_description.get_name()
+    )
+
+    configuration={}
+    configuration["frame_rate"] = meta_description.get_frame_rate()
+    configuration["resolution"] = meta_description.get_resolution()
+    configuration["horizontal_fov"] = meta_description.get_horizontal_fov()
+    configuration["video_format"] = meta_description.get_video_format()
+
+    geometry={}
+    geometry["parent_link"]=meta_description.get_parent_link()
+    geometry["xyz"]=meta_description.get_xyz()
+    geometry["rpy"]=meta_description.get_rpy_deg()
+
+
+    return romea_camera_description.urdf(
+        robot_urdf_prefix(robot_namespace),
+        mode,
+        meta_description.get_name(),
+        meta_description.get_type(),
+        meta_description.get_model(),
+        configuration,
+        geometry,
+        ros_namespace
+    )
---
+++ romea_camera_description/python/romea_camera_description.py
@@ -0,0 +1,132 @@
+# Copyright 2022 INRAE, French National Research Institute for Agriculture, Food and Environment
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import xacro
+import yaml
+
+from ament_index_python.packages import get_package_share_directory
+
+from romea_common_description import (
+    evaluate_parameter,
+    evaluate_parameter_from_list,
+    evaluate_parameter_from_range,
+    get_device_geometry_file_path,
+    get_device_specifications,
+    save_device_specifications_file,
+)
+
+
+def image_width(resolution):
+    return int(resolution.split("x")[0])
+
+
+def image_height(resolution):
+    return int(resolution.split("x")[1])
+
+
+def evaluate_camera_parameter(
+    camera_type, camera_model, specifications, user_configuration, parameter_name, key_value=None
+):
+    return evaluate_parameter(
+        "camera", camera_type, camera_model, specifications, user_configuration, parameter_name, key_value
+    )
+
+
+def evaluate_camera_parameter_from_list(
+    camera_type, camera_model, specifications, user_configuration, parameter_name, key_value=None
+):
+    return evaluate_parameter_from_list(
+        "camera", camera_type, camera_model, specifications, user_configuration, parameter_name, key_value
+    )
+
+
+def evaluate_camera_parameter_from_range(
+    camera_type, camera_model, specifications, user_configuration, parameter_name, key_value=None
+):
+    return evaluate_parameter_from_range(
+        "camera", camera_type, camera_model, specifications, user_configuration, parameter_name, key_value
+    )
+
+
+def get_camera_geometry_file_path(camera_type, camera_model):
+    return get_device_geometry_file_path("camera", camera_type, camera_model)
+
+
+def get_camera_specifications(camera_type, camera_model):
+    return get_device_specifications("camera", camera_type, camera_model)
+
+
+def get_camera_complete_configuration(camera_type, camera_model, user_configuration):
+    if camera_type == "axis":
+        return get_axis_complete_configuration(camera_model, user_configuration)
+
+    raise LookupError("Cannot get camera configuration")
+
+
+def get_axis_complete_configuration(camera_model, user_configuration):
+
+    specifications = get_camera_specifications("axis", camera_model)
+    
+    configuration = {}
+    resolution = evaluate_camera_parameter_from_list(
+        "axis", camera_model, specifications, user_configuration, "resolution")
+
+    configuration["image_width"] = image_width(resolution)
+    configuration["image_height"] = image_height(resolution)
+
+    configuration["horizontal_fov"] = evaluate_camera_parameter_from_range(
+        "axis", camera_model, specifications, user_configuration, "horizontal_fov"
+    )
+
+    configuration["frame_rate"] = evaluate_camera_parameter(
+        "axis", camera_model, specifications, user_configuration, "frame_rate", resolution
+    )
+    # configuration["video_mode"] = evaluate_camera_parameter(
+    #     "axis", camera_model, specifications, user_configuration, "video_mode", resolution
+    # )
+    configuration["video_format"] = evaluate_camera_parameter_from_list(
+        "axis", camera_model, specifications, user_configuration, "video_format"
+    )
+
+    return configuration
+
+
+def urdf(prefix, mode, name, type, model, user_configuration, user_geometry, ros_namespace):
+
+    complete_configuration = get_camera_complete_configuration(type, model, user_configuration)
+    complete_configuration_yaml_file = save_device_specifications_file(prefix, name, complete_configuration)
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",complete_configuration_yaml_file)
+    geometry_yaml_file = get_camera_geometry_file_path(type, model)
+
+    xacro_file = get_package_share_directory("romea_camera_description") + "/urdf/camera.xacro.urdf"
+
+    urdf_xml = xacro.process_file(
+        xacro_file,
+        mappings={
+            "prefix": prefix,
+            "mode": mode,
+            "name": name,
+            "type": type,
+            "model": model,
+            "sensor_config_yaml_file": complete_configuration_yaml_file,
+            "geometry_config_yaml_file": geometry_yaml_file,
+            "parent_link": user_geometry["parent_link"],
+            "xyz": " ".join(map(str, user_geometry["xyz"])),
+            "rpy": " ".join(map(str, user_geometry["rpy"])),
+            "mesh_visual": str(True),
+            "ros_namespace": ros_namespace,
+        },
+    )
+    return urdf_xml.toprettyxml()
